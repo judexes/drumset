47c6e73bf14b53c2b2aebe5bb935cd2e8af375dc
jude okoroafor <judeokoroafor@gmail.com>
2020-12-05 23:58:34 +0100

    sound plays FROM 0.00

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-    if (!audio) return;
+  if (!audio) return;
+  audio.currentTime = 0; //SET AUDIO TIME TO ALWAYS START AT 0 ON BUTTON CLICK
+  audio.play();
 
-    audio.play();
-
-    console.log(audio);
-} )
+  console.log(audio);
+});
