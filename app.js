8549f332b854e46a148969c292bbd7bb3ea31bb2
jude okoroafor <judeokoroafor@gmail.com>
2020-12-05 23:54:20 +0100

    sound plays

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    if (!audio) return;
+
+    audio.play();
+
     console.log(audio);
 } )
