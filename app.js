02d7e7620bf1036292cbb2ac230a9256a20ba7bc
jude okoroafor <judeokoroafor@gmail.com>
2020-12-09 01:09:31 +0100

    update

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
   if (!audio) return;
+
   audio.currentTime = 0; //SET AUDIO TIME TO ALWAYS START AT 0 ON BUTTON CLICK
+
   audio.play();
 
-  console.log(audio);
+  console.log(key);
+
+  key.classList.add('playing');
+  key.classList.remove('playing');
 });
